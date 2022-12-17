dcb57b6fb76a3828ab8f62757f081d9efcb40409
Gislaine Frandalozo <frandalozo.gislaine@gmail.com>
2022-12-16 21:19:11 -0300

    fix message input

--- resources/js/Components/InputMessage.jsx
+++ resources/js/Components/InputMessage.jsx
@@ -14,7 +14,7 @@ export default function InputMessage(props) {
     };
 
     return (
-        <div className="bg-white mt-3 dark:bg-gray-800 overflow-hidden">
+        <div className="bg-white sm:rounded-lg sm:my-3 sm:mx-4 dark:bg-gray-800 fixed bottom-0 left-0 right-0">
             <form className="flex p-3 items-stretch" onSubmit={submit}>
                 <div className="text-gray-900 w-5/6 flex dark:text-gray-100">
                     <input
@@ -24,7 +24,7 @@ export default function InputMessage(props) {
                         value={data.message}
                         onChange={e => setData('message', e.target.value)} />
                 </div>
-                <div className="self-center w-1/6 grid justify-items-center">
+                <div className="self-center px-1 w-1/6 grid justify-items-center">
                     <PrimaryButton className="h-min w-min" processing={processing}>
                         ok
                     </PrimaryButton>
--- resources/js/Layouts/BodyLayout.jsx
+++ resources/js/Layouts/BodyLayout.jsx
@@ -1,6 +1,6 @@
 export default function Body({children}) {
     return(
-        <div className="h-screen sm:h-full sm:mx-6 sm:mt-6 bg-white dark:bg-white place-content-between flex flex-col ">
+        <div className="h-screen mt-6 bg-white dark:bg-white relative flex flex-col place-content-between">
             {children}
         </div>
     )
