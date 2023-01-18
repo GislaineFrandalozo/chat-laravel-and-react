46d87a0d232c5248f3dbaf54b035d05b55ca3cec
Gislaine Frandalozo <frandalozo.gislaine@gmail.com>
2023-01-18 15:41:14 -0300

    ex-03

--- resources/js/Components/Quote.jsx
+++ resources/js/Components/Quote.jsx
@@ -1,7 +1,5 @@
 import { useCallback } from 'react';
 
-console.log("xxx")
-
 export var channelQuote = (setQuote) => {
     Echo.private('Quote').listen('QuoteBroadcasting', useCallback(setQuote));
 }
